523c54870e04bcfe7add4af1aae70f5f097a5982
Carlos Gabriel <82345279+eucgabriel@users.noreply.github.com>
2022-03-23 21:45:36 -0300

    final do dia

--- teste/core/views.py
+++ teste/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    template_name = 'index.html'
+    context = {
+        'nome' : ' UGB'
+    }
+    return render(request, template_name, context)
--- teste/teste/urls.py
+++ teste/teste/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from core.views import index
 urlpatterns = [
+    path('padaria/', index),
     path('admin/', admin.site.urls),
 ]
