660d36f9a89119ff96f86e72d8a99f93767bab57
Carlos Gabriel <82345279+eucgabriel@users.noreply.github.com>
2022-03-31 19:33:09 -0300

    atualização

--- Estrutura de Dados/teste.py
+++ Estrutura de Dados/teste.py
@@ -1,13 +1,1 @@
-def buscaSeq(x, proc):
-    for i in range(0, len(x)):
-        if x[i] == proc:
-            return i
-    return -1
-
-x = [2, 1, 4, 5, 7, 9, 10, 8, 3]
-i = 0
-k = buscaSeq(x, 0)
-if k >= 0:
-    print('''{} está na posição {} do vetor'''.format(i, k))
-else:
-    print('''{} não se encontra no vetor'''.format(i))
+from django.db import forms 
