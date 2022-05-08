41e638bac692f4ec66535e078c05b0032883f631
Carlos Gabriel <canalcarlosgabriel1407@gmail.com>
2022-05-08 18:06:58 -0300

    trocando os nomes

--- Programação 2/revisão/aula 02.py
+++ Programação 2/revisão/aula 02.py
@@ -1,9 +1,9 @@
 from tank import Tank
 
 tanks = {
-    'a': Tank('Alice'),
-    'b': Tank('Bob'),
-    'c': Tank('Carol')
+    'a': Tank('Felipe'),
+    'b': Tank('CG'),
+    'c': Tank('Matheus')
 }
 
 alive_tanks = len(tanks)
